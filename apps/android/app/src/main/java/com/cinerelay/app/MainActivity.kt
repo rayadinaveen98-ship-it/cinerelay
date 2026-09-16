f8c9dd616a9c9e517caba32c3b55bce524fe2c3f
rayadinaveen98-ship-it <rayadinaveen98@gmail.com>
2026-09-16 18:52:10 +0530

    feat(android): add Compose host activity

---
+++ apps/android/app/src/main/java/com/cinerelay/app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.cinerelay.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
+import com.cinerelay.app.ui.CineRelayApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContent { CineRelayApp() }
+    }
+}
